3d312d5e66c354390bd920424103e2b8d8650dc5
weiyuet <weiyuet@gmail.com>
2022-07-05 03:45:52 +0800

    Added students and teachers data, and updated README

---
+++ code/students-and-teachers-in-schools.R
@@ -0,0 +1,35 @@
+# Loading libraries
+library(tidyverse)
+library(scales)
+
+# Loading data
+students_and_teachers_primary_schools <- read_csv("data/students-and-teachers-in-schools/students-and-teachers-primary-schools.csv")
+
+# Wrangling data
+students_and_teachers_primary_schools %>% 
+  filter(sex == "MF", school_type == "Govt") %>% 
+  ggplot(aes(x = year, y = students_pri)) +
+  geom_line() +
+  geom_point() +
+  theme_light() +
+  scale_x_continuous(breaks = seq(1980, 2020, 5)) +
+  scale_y_continuous(labels = label_number(big.mark = ",")) +
+  labs(x = "", y = "",
+       title = "Primary school students (M&F) in Govt schools",
+       caption = "Source: data.gov.sg\nGraphic: @weiyuet")
+  
+ggsave("figures/primary-school-students-in-govt-schools.png", width = 8, height = 6)
+
+students_and_teachers_primary_schools %>% 
+  filter(sex == "MF", school_type == "Aided") %>% 
+  ggplot(aes(x = year, y = students_pri)) +
+  geom_line() +
+  geom_point() +
+  theme_light() +
+  scale_x_continuous(breaks = seq(1980, 2020, 5)) +
+  scale_y_continuous(labels = label_number(big.mark = ",")) +
+  labs(x = "", y = "",
+       title = "Primary school students (M&F) in Aided schools",
+       caption = "Source: data.gov.sg\nGraphic: @weiyuet")
+
+ggsave("figures/primary-school-students-in-aided-schools.png", width = 8, height = 6)
