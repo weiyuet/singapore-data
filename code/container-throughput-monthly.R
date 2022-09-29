4d05210e6367fa8032014ce9fd54e7733f7c1de3
weiyuet <weiyuet@gmail.com>
2022-09-29 17:52:46 +0800

    Added infectious disease data

--- code/container-throughput-monthly.R
+++ code/container-throughput-monthly.R
@@ -19,9 +19,9 @@ container_throughput_monthly %>% separate(month, c("year", "month")) %>%
                      expand = c(0, 0)) +
   theme_classic() +
   labs(x = "", y ="",
-       title = "Container Throughput (Yearly Total)",
+       title = "Container Throughput (Total)",
        subtitle = "'000 Twenty-foot equivalent units",
-       caption = "Source: data.gov.sg / Maritime and Port Authority of Singapore\nGraphic: @weiyuet")
+       caption = "Data: data.gov.sg / Maritime and Port Authority of Singapore\nGraphic: @weiyuet")
 
 # Save png
 ggsave("figures/container-throughput-yearly-total.png", width = 10, height = 6.5)
