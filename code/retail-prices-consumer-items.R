5ccf0f93e95cd33d3b0f7fbebba7ff35fbbd18f1
weiyuet <weiyuet@gmail.com>
2022-10-11 17:46:47 +0800

    Added retail prices of typical consumer items

---
+++ code/retail-prices-consumer-items.R
@@ -0,0 +1,55 @@
+library(tidyverse)
+library(scales)
+
+raw_prices <- read_csv("data/retail-prices-consumer-items/M212951-table.csv", skip = 10)
+
+# Wrangle
+# Remove bottom rows
+prices <- raw_prices[-c(70:86), ]
+
+# Rename item column
+prices <- prices %>% 
+  rename(item = `Data Series`)
+
+# Convert character columns to numeric
+prices <- prices %>%
+  mutate(across(10:13, as.numeric))
+
+# Change to tidy format
+tidy_prices <- prices %>%
+  pivot_longer(2:13,
+               names_to = "year",
+               values_to = "price")
+
+# Change year to numeric
+tidy_prices$year <- as.numeric(as.character(tidy_prices$year))
+
+# Visualize
+# Price of typical dishes
+items_selected <- c("Fishball Noodle (Per Bowl)", "Mee Rebus (Per Bowl)", "Chicken Rice (Per Plate)", "Chicken Nasi Briyani (Per Plate)", "Economical Rice (1 Meat & 2 Vegetables) (Per Plate)", "Roti Prata (Plain) (Per Piece)", "Fried Carrot Cake (Per Plate)", "Ice Kachang (Per Bowl)")
+
+tidy_prices %>% 
+  filter(item %in% items_selected) %>% 
+  ggplot(aes(x = year, y = price, colour = item)) +
+  geom_line(show.legend = FALSE) +
+  facet_wrap(~item, scales = "free_y") +
+  scale_x_continuous(limits = c(2014, 2022))
+
+# Price of selected household groceries
+items_selected <- c("Premium Thai Rice (Per 5 Kilogram)", "Ordinary White Bread (Per 400 Gram)", "Instant Noodles (Per 5 Packets)", "Whole Chicken, Chilled (Per Kilogram)", "Hen Eggs (Per 10)", "Chinese Kale (Kailan) (Per Kilogram)")
+
+tidy_prices %>% 
+  filter(item %in% items_selected) %>% 
+  ggplot(aes(x = year, y = price, colour = item)) +
+  geom_line(show.legend = FALSE) +
+  facet_wrap(~item, scales = "free_y")
+
+# Price of fuel
+items_selected <- c("Diesel (Per Litre)", "Petrol, 98 Octane (Per Litre)", "Petrol, 95 Octane (Per Litre)", "Petrol, 92 Octane (Per Litre)", "Liquefied Petroleum Gas (LPG) (Per Kilogram)")
+
+tidy_prices %>% 
+  filter(item %in% items_selected) %>% 
+  ggplot(aes(x = year, y = price, colour = item)) +
+  geom_line(show.legend = FALSE) +
+  facet_wrap(~item, scales = "free_y")
+  
