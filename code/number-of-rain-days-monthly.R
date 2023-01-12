94aaba3ebbb2689b5b5cd7ad39090bc3d74aa2d4
weiyuet <weiyuet@gmail.com>
2023-01-12 20:57:37 +0800

    Updated plot aesthetics

--- code/number-of-rain-days-monthly.R
+++ code/number-of-rain-days-monthly.R
@@ -21,7 +21,7 @@ number_of_rain_days_monthly = transform(number_of_rain_days_monthly,
 
 #### Visualize ####
 # How many rain days are there in a month?
-number_of_rain_days_monthly %>% 
+number_of_rain_days_monthly %>%
   ggplot(aes(x = month,
              y = no_of_rainy_days)) +
   geom_point() +
@@ -30,14 +30,17 @@ number_of_rain_days_monthly %>%
              linetype = "dashed",
              colour = "red") +
   facet_wrap(vars(year)) +
-  scale_x_continuous(breaks = seq(1, 12, 4),
-                     labels = month.abb[seq(1, 12, 4)]) +
+  scale_x_continuous(breaks = seq(1, 12, 2),
+                     labels = month.abb[seq(1, 12, 2)]) +
   scale_y_continuous(breaks = seq(0, 30, 5)) +
-  theme_classic() +
   labs(x = "",
        y = "",
        title = "Number of Rain Days per Month - recorded at Changi Climate Station",
-       caption = "Data: National Environment Agency (data.gov.sg) | Graphic: @weiyuet")
+       caption = "Data: National Environment Agency (data.gov.sg) | Graphic: @weiyuet") +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 90,
+                                   vjust = 0.5,
+                                   hjust = 1))
 
 #### Save image ####
 ggsave("figures/number-of-rain-days-monthly.png", width = 8, height = 8)
