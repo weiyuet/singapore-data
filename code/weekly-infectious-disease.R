21fd69f8ef25d713c3dc6d46958e4c8566226bd2
weiyuet <weiyuet@gmail.com>
2023-01-20 00:46:40 +0800

    Updated with latest available data

--- code/weekly-infectious-disease.R
+++ code/weekly-infectious-disease.R
@@ -1,6 +1,6 @@
-#########################################
-# Infectious Disease Weekly Case Counts #
-#########################################
+##########################################
+# Infectious Disease Weekly Case Numbers #
+##########################################
 
 #### Setup ####
 library(tidyverse)
@@ -25,27 +25,36 @@ weekly_infectious_disease <- weekly_infectious_disease %>%
          week = "col 2",
          cases = "no._of_cases")
 
+# Extract numbers from week column
+weekly_infectious_disease$week <- as.numeric(str_extract(weekly_infectious_disease$week,
+                                                         "[0-9]+"))
+
+# Convert year into numeric
+weekly_infectious_disease <- weekly_infectious_disease %>% 
+  mutate(year = as.numeric(year))
+
 # Clean HFMD and Hand, Foot Mouth Disease names
 weekly_infectious_disease <- weekly_infectious_disease %>% 
   mutate(disease = case_when(disease == "HFMD" ~ "Hand, Foot Mouth Disease",
                              TRUE ~ disease))
 
 #### Visualize ####
-# Plot weekly case numbers of Dengue Fever from 2012 to 2021
+# Plot weekly case numbers of Dengue Fever
 weekly_infectious_disease %>%
   filter(disease == "Dengue Fever") %>%
   ggplot(aes(x = week,
              y = cases)) +
   geom_col() +
   facet_wrap(vars(year)) +
+  scale_x_continuous(expand = c(0, 0)) +
   scale_y_continuous(expand = c(0, 0)) +
-  labs(x = "",
+  labs(x = "Week#",
        y = "",
        title = glue("Weekly Case Numbers of Dengue Fever ({min(weekly_infectious_disease$year)}-{max(weekly_infectious_disease$year)}) in Singapore"),
-       subtitle = "June seems to be the peak month, with 2020 being a particularly bad year",
+       subtitle = "June seems to be the peak month; 2020 and 2022 being very bad years",
        caption = "Data: Ministry of Health (data.gov.sg) | Graphic: @weiyuet") +
   theme_classic() +
-  theme(axis.text.x = element_blank(),
+  theme(axis.text.x = element_text(),
         axis.ticks.x = element_blank())
 
 #### Save image ####
