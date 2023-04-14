9fc5e4b22b3419cda4595e2bbc5632cdb9c08d19
weiyuet <weiyuet@gmail.com>
2023-04-15 01:31:53 +0800

    Updated with latest available data

--- code/container-throughput-monthly.R
+++ code/container-throughput-monthly.R
@@ -6,17 +6,19 @@
 library(tidyverse)
 library(glue)
 
-#### Load data ####
+#### Load Data ####
 container_throughput_monthly <- read_csv("data/container-throughput-monthly-total/container-throughput-monthly.csv")
 
 #### Wrangle ####
 # Separate month and year
 container_throughput_monthly <- container_throughput_monthly %>% 
-  separate(month, c("year", "month"))
+  separate(month, 
+           c("year", "month"))
 
 # Convert year and month into numeric
 container_throughput_monthly <- container_throughput_monthly %>% 
-  mutate(across(1:2, as.numeric))
+  mutate(across(1:2, 
+                as.numeric))
 
 #### Visualize ####
 # How many containers go through Singapore each month?
@@ -27,15 +29,16 @@ container_throughput_monthly %>%
   facet_wrap(vars(year)) +
   scale_x_continuous(breaks = seq(1:12),
                      labels = month.abb[seq(1:12)]) +
-  scale_y_continuous(expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0.1, 0)) +
   labs(x = "",
        y = "",
-       title = glue("Container Throughput ({min(container_throughput_monthly$year)}-{max(container_throughput_monthly$year)}) '000 Twenty-foot equivalent units"),
+       title = glue("Container Throughput ({min(container_throughput_monthly$year)}-{max(container_throughput_monthly$year)})"),
+       subtitle = "'000 Twenty-foot equivalent units",
        caption = "Data: Maritime and Port Authority of Singapore (data.gov.sg) | Graphic: @weiyuet") +
   theme_classic() +
   theme(axis.text.x = element_text(angle = 90,
                                    vjust = 0.5,
                                    hjust = 1))
 
-#### Save image ####
+#### Save Image ####
 ggsave("figures/container-throughput-monthly.png", width = 8.5, height = 7)
